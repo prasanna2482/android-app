b43e1c60149806b62e8207d48890738a2ee105c9
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-30 19:10:18 +0900

    Revert "Remove desugar."
    This reverts commit 81ae9e3ec80b1af3862da38e1133fcd538e8ffe3.

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
@@ -20,6 +20,7 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.provideDelegate
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
@@ -37,9 +38,17 @@ internal fun Project.configureKotlinAndroid(
         defaultConfig {
             minSdk = 21
         }
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+        }
     }
 
     configureKotlin<KotlinAndroidProjectExtension>()
+
+    dependencies {
+        add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+    }
 }
 
 /**
