5f6c02b648531c367fa2f18f0af1ae5a409fdfee
Prasanna R <prasanna.r@opsera.io>
2025-03-28 17:26:09 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,6 @@ buildTypes {
 
 dependencies {
     classpath "com.android.tools.build:gradle:8.1.0"
-    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
     implementation(projects.feature.interests)
     implementation(projects.feature.foryou)
     implementation(projects.feature.bookmarks)
