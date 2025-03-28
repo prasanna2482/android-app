71da55efab39dbf42f9eeb1affbd3214571a19f8
Prasanna R <prasanna.r@opsera.io>
2025-03-28 20:05:35 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,10 @@ android {
     }
 signingConfigs {
     create("release") {
-                storeFile = file(project.findProperty("KEYSTORE_FILE")
-                storePassword = project.findProperty("KEYSTORE_PASSWORD")
-                keyAlias = project.findProperty("KEY_ALIAS")
-                keyPassword = project.findProperty("KEY_PASSWORD")
+                storeFile = file($project.findProperty("KEYSTORE_FILE")
+                storePassword = $project.findProperty("KEYSTORE_PASSWORD")
+                keyAlias = $project.findProperty("KEY_ALIAS")
+                keyPassword = $project.findProperty("KEY_PASSWORD")
             }
         }
     
