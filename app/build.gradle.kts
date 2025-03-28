501fec2c95dd62559581ecc8fa78c1d1e4f7d4b7
Prasanna R <prasanna.r@opsera.io>
2025-03-28 17:43:53 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,6 @@ plugins {
     alias(libs.plugins.nowinandroid.android.application.firebase)
     alias(libs.plugins.nowinandroid.hilt)
     id("com.google.android.gms.oss-licenses-plugin")
-    alias(compileSdkVersion 3libs.plugins.baselineprofile)
     alias(libs.plugins.roborazzi)
     alias(libs.plugins.kotlin.serialization)
 }
